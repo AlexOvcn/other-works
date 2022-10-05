c084586618cd1b30a98a02c605e9d604e6b470f8
Alex <komfort-yut@mail.ru>
2022-10-05 22:33:26 +0500

    initial commit

---
+++ modelExtension/index.d.ts
@@ -0,0 +1,198 @@
+//? Библиотека для комфортной работы с запросами MySQL. V 1.0.0
+
+//* импортируем интерфейсы: ошибки MySQL, пула, ответ бд при запросах отличных от select
+import { Pool, OkPacket } from 'mysql';
+
+//* определение типа доступных знаков сравнения 
+type ComparisonSignsType = '='|'!='|'<>'|'<'|'>'|'<='|'>='|'LIKE';
+//* определение типа доступных знаков арифметических операций 
+type SignsArithmeticOperationsType = '+'|'-'|'/'|'*';
+//* определение типа доступных методов соединений
+type JoinMethodsType = 'INNER'|'LEFT'|'RIGHT';
+
+//* описываем расширение модели к базе данных для работы с запросами к ней
+export abstract class ModelExtension {
+
+    /**
+     * Определение обьекта с полями, нужными для определенных методов
+     * @param open_pool_connections Открытый пул соединений для отправки запросов, обязательный параметр [Pool]
+     * @param table Указание текущей таблицы, используется в большинстве методов [string]
+     */
+    protected abstract options: {
+        // открытый пул соединений
+        open_pool_connections: Pool
+        // текущая таблица
+        table?: string
+    }
+
+    /**
+     * Свойство представляет из себя строку запроса, которая будет собираться по частям в процессе работы методов класса
+     */
+    private query: string;
+    /**
+     * Свойство представляет из себя массив значений, который будет пополняться в процессе работы методов класса
+     */
+    private values: (string | number)[];
+
+    /**
+     * Основная функция для непосредственного взаимодействия с базой данных
+     * @param query Строка запроса
+     * @param values Массив значений
+     * @returns Возвращается промис в котором первым аргументом попадает либо результат ответа на запрос либо ошибка MysqlError
+     */
+    private executeQuery(query: string, values: (string | number)[]): Promise<OkPacket|any[]>
+
+    /**
+     * Метод для проверки некоторых ключевых строк перед вставкой в запрос
+     * @param str Строка для проверки
+     * @returns Возвращает проверяемую строку или null
+     */
+    private checkingSomeSymbols(str: string|null|undefined): string|null
+
+    /**
+     * Метод для создания неограниченного количества вопросительных знаков для вставки в предварительный запрос
+     * @param quantity Количество вопросительных меток
+     * @param sign Знак запятой, если нужно разделение между значениями
+     * @param typeOfValue Тип значения для вставки (как поле или как значение)
+     * @param modeWithBrackets Если нужно объединение в группы, к примеру для работы с INSERT INTO VALUES (Опционально) (По умолч. = false)
+     * @param frequency Количество значений в одной группе (Опционально) (По умолч. = 1)
+     * @returns Возвращается сконкатенированная строка из знаков вопроса разделенных запятой, кроме последнего знака
+     */
+    private creatorOfQuestionMarks(quantity: number, sign: boolean, typeOfValue: 'field'|'value', modeWithBrackets?: boolean, frequency?: number): string
+
+    /**
+     * Метод для создания неограниченного количества вопросительных знаков для вставки в предварительный запрос (специально для инструкции SET)
+     * @param quantity Количество присваиваемых блоком
+     * @param useCurrentValue Передается массив соразмерный количеству вводимых значений, в массиве указать знаки арифметических операций, если нужно произвести операцию с текущим значением поля(+ - * /), к примеру currentField = currentField + 1, или передать null любому из значений, если подразумевается обычное присваение currentField = someValue
+     * @returns Возвращается сконкатенированная строка из присваиваний со знаками вопроса разделенных запятой, кроме последнего присваивания
+     */
+    private creatorOfQuestionMarksForSET(quantity: number, useCurrentValue: (SignsArithmeticOperationsType|null)[]): string
+
+    /**
+     * Метод для создания неограниченного количества вопросительных знаков для вставки в предварительный запрос (специально для мест где необходимо выбрать поле с уточнением таблицы)
+     * @param fields Передается массив из строки(название таблицы) или массива с двумя строками(названием таблицы и названием столбца)
+     * @returns Возвращается сконкатенированная строка из знаков вопроса разделенных запятой, кроме последнего знака
+     */
+    private creatorOfQuestionMarksForTableWithColumn(fields: (string|[string,string])[]): string
+
+    /**
+     * Метод для завершения построения запроса и отправки его в базу данных
+     * @param showDetails Позволяет показать детали запроса сконструированного этим классом (опционально) (по умолчанию = false)
+     * @returns Возвращается промис в котором первым аргументом попадает либо результат ответа на запрос либо ошибка
+     */
+    execute(showDetails?: boolean): Promise<OkPacket|any[]>
+
+    /**
+     * Метод для сортировки по значени(ю/ям) в указанн(ом/ых) пол(е/ях)
+     * @param field Поле сортируемого значения
+     * @param sign Знак сравнения
+     * @param value Сравниваемое значение
+     * @param additionalCondition Добавление еще одного блока сравнения с разделителем OR, AND (Опционально)
+     * @param field2 Поле сортируемого значения (Опционально)
+     * @param sign2 Знак сравнения (Опционально)
+     * @param value2 Сравниваемое значение (Опционально)
+     * @param additionalCondition2 Добавление еще одного блока сравнения с разделителем OR, AND (Опционально)
+     * @param field3 Поле сортируемого значения (Опционально)
+     * @param sign3 Знак сравнения (Опционально)
+     * @param value3 Сравниваемое значение (Опционально)
+     * @returns Возвращает контекст текущего класса
+     */
+    where(field: string, sign: ComparisonSignsType, value: string|number): this
+    where(field: string, sign: ComparisonSignsType, value: string|number, additionalCondition: 'AND'|'OR', field2: string, sign2: ComparisonSignsType, value2: string|number): this
+    where(field: string, sign: ComparisonSignsType, value: string|number, additionalCondition: 'AND'|'OR', field2: string, sign2: ComparisonSignsType, value2: string|number, additionalCondition2: 'AND'|'OR', field3: string, sign3: ComparisonSignsType, value3: string|number): this
+
+    /**
+     * Метод для группировки значений в столбцах
+     * @note Использование агрегатных функций не реализовано, обратитесь к методу ownQuery
+     * @param fields Поля, которые будут группироваться
+     * @returns Возвращает контекст текущего класса
+     */
+    groupBy(...fields: string[]): this
+
+    /**
+     * Метод для выстраивания значений таблицы в определенном порядке
+     * @param fields Поля по которым будет определятся порядок
+     * @param method Метод выстраивания значений (Опционально) (по умолчанию = 'ASC')
+     * @returns Возвращает контекст текущего класса
+     */
+    orderBy(fields: string[], method?: 'ASC'|'DESC'|'RANDOM'): this
+
+    /**
+     * Метод для лимитирования результата выборки
+     * @param quantityOfReturnRows Количество возвращаемых строк - лимит
+     * @param quantityOfRowsToSkip Количество пропускаемых строк перед началом возвращаемых (Опционально) (по умолчанию = 0)
+     * @returns Возвращает контекст текущего класса
+     */
+    limit(quantityOfReturnRows: number, quantityOfRowsToSkip?: number): this
+
+    /**
+     * Метод для выбора возвращаемых полей в результирующей выборке
+     * @note Использование агрегатных функций не реализовано, обратитесь к методу ownQuery
+     * @param fields Возвращаемые поля, если нужно уточнить в какой таблице поле находится, то можно передать массив из двух строк(названия таблицы и названия столбца), иначе указать только название столбца
+     * @returns Возвращает контекст текущего класса
+     */
+    select(...fields: (string|[string,string])[]): this
+
+    /**
+     * Метод для выборки данных из нескольких таблиц, результат складывается в результирующий набор
+     * @note При указании сравниваемого поля можно указать название поля или же можно передать массив с двумя строками - названием таблицы и поля соответственно
+     * @param method Метод работы JOIN
+     * @param tables Присоединяемые таблицы
+     * @param firstField Сравниваемое поле
+     * @param sign Знак сравнения
+     * @param secondField Сравниваемое поле
+     * @param additionalCondition Добавление еще одного блока сравнения с разделителем OR, AND (Опционально)
+     * @param firstField2 Сравниваемое поле (Опционально)
+     * @param sign2 Знак сравнения (Опционально)
+     * @param secondField2 Сравниваемое поле (Опционально)
+     * @param additionalCondition2 Добавление еще одного блока сравнения с разделителем OR, AND (Опционально)
+     * @param firstField3 Сравниваемое поле (Опционально)
+     * @param sign3 Знак сравнения (Опционально)
+     * @param secondField3 Сравниваемое поле (Опционально)
+     * @returns Возвращает контекст текущего класса
+     */
+    join(method: JoinMethodsType, tables: string[], firstField: string|[string, string], sign: ComparisonSignsType, secondField: string|[string, string]): this
+    join(method: JoinMethodsType, tables: string[], firstField: string|[string, string], sign: ComparisonSignsType, secondField: string|[string, string], additionalCondition: 'AND'|'OR', firstField2: string|[string, string], sign2: ComparisonSignsType, secondField2: string|[string, string]): this
+    join(method: JoinMethodsType, tables: string[], firstField: string|[string, string], sign: ComparisonSignsType, secondField: string|[string, string], additionalCondition: 'AND'|'OR', firstField2: string|[string, string], sign2: ComparisonSignsType, secondField2: string|[string, string], additionalCondition2: 'AND'|'OR', firstField3: string|[string, string], sign3: ComparisonSignsType, secondField3: string|[string, string]): this
+
+    /**
+     * Метод для вставки данных в бд
+     * @param fields Поля в которые будут вставляться значения
+     * @param values Вставляемые значения (запись имеет следующий вид, в массив записываются массивы, один массив - это одна затрагиваемая строка в бд)
+     * @returns Возвращает контекст текущего класса
+     */
+    insertValues(fields: string[], values: (string|number)[][]): this
+
+    /**
+     * Метод для 'частичного' обновления данных в таблице бд (если обновляемая строка не найдена - новая не создается)
+     * @note Многотабличное обновление не реализовано, обратитесь к методу ownQuery
+     * @param fields Поля в которых будут обновляться значения
+     * @param values Вставляемые значения
+     * @param useCurrentValue Передается массив соразмерный количеству обновляемых значений, в массиве указать знаки арифметических операций, если нужно произвести операцию с текущим значением поля(+ - * /), к примеру currentField = currentField + 1, или передать null любому из значений, если подразумевается обычное присваение currentField = someValue
+     * @returns Возвращает контекст текущего класса
+     */
+    updateSet(fields: string[], values: (string|number)[], useCurrentValue: (SignsArithmeticOperationsType|null)[]): this
+
+    /**
+     * Метод для полного обновления данных в таблице бд, если какое-то поле не указали при выборе изменяемых полей, то значение в этих полях сбросится к значению по умолчанию либо появится ошибка при ограниченях в бд для определенных полей (если обновляемая строка не найдена - создается новая)
+     * @param fields Поля которые учавствуют в обновлении или вставке
+     * @param values Обновляемые или вставляемые значения (запись имеет следующий вид, в массив записываются массивы, один массив - это одна затрагиваемая строка в бд)
+     * @returns Возвращает контекст текущего класса
+     */
+    replaceValues(fields: string[], values: (string|number)[][]): this
+
+    /**
+     * Метод для удаления данных
+     * @attention Работает в таблице, которая была указана в модели
+     * @returns Возвращает контекст текущего класса
+     */
+    delete(): this
+
+    /**
+     * Метод для создания любого запроса MySQL (так же он может просто дополнить запрос)
+     * @param query Строка запроса, в которой нужно проставить метки для последующих вставок значений (?? - поле, ? - значение)
+     * @param values Значения, которые по очереди будут вставляться в строку запроса
+     * @returns Возвращает контекст текущего класса
+     */
+    ownQuery(query: string, values: (string|number)[]): this
+}
---
+++ modelExtension/index.js
@@ -0,0 +1,182 @@
+//? Библиотека для комфортной работы с запросами MySQL. V 1.0.0
+
+exports.ModelExtension = class ModelExtension {
+
+    options = {}
+
+    #query = '';
+    #values = [];
+
+    async #executeQuery(query, values) {
+
+        //* создание асинхронного выполнения запроса
+        let result = await (new Promise((resolve, reject) => {
+
+            if (!this.options.open_pool_connections) {return reject(new Error('Не назначено свойство "open_pool_connections" в обьекте options! А оно является обязательным.'))}
+
+            //* получение свободного соединения из пула 
+            this.options.open_pool_connections.getConnection(function(err, connection) {
+                if (err) return reject(err); //* нет соединения
+
+                //* использование соединения
+                connection.query(query, values, function (error, results) {
+
+                    //* когда запрос выполнен, нужно отпустить подключение обратно в пул
+                    connection.release();
+                    //* если произошла ошибка в запросе, обрабатываем после релиза
+                    if (error) return reject(error);
+                    //* возвращаем результат с разрешившимся промисом
+                    resolve(results);
+                
+                    //* здесь соединение уже недоступно, так как оно было возвращено в пул
+                });
+
+            });
+
+        }));
+        return result;
+    }
+
+    #checkingSomeSymbols(str) {
+        switch (str) {
+            case '=': return str;
+            case '!=': return str;
+            case '<>': return str;
+            case '<': return str;
+            case '>': return str;
+            case '<=': return str;
+            case '>=': return str;
+            case 'AND': return str;
+            case 'OR': return str;
+            case 'LIKE': return str;
+            case '+': return str;
+            case '-': return str;
+            case '*': return str;
+            case '/': return str;
+            case 'ASC': return str;
+            case 'DESC': return str;
+            case 'RANDOM': return 'rand()';
+            case 'INNER': return str;
+            case 'LEFT': return str;
+            case 'RIGHT': return str;
+            default: return null;
+        }
+    }
+
+    #creatorOfQuestionMarks(quantity, sign, typeOfValue, modeWithBrackets = false, frequency = 1) {
+        let str = '';
+        const typeOfDesignation = { "field": '??', "value": '?' };
+        for (let i = 0; i < quantity; i++) {
+            if (modeWithBrackets && i === 0) str += '(';
+            i+1 === quantity ? str += typeOfDesignation[typeOfValue] : str += `${typeOfDesignation[typeOfValue]}${modeWithBrackets && (i+1) % frequency === 0 ? ')' : ''}${sign ? ',': ''} ${modeWithBrackets && (i+1) % frequency === 0 ? '(' : ''}`;
+            if (modeWithBrackets && i+1 === quantity) str += ')';
+        }
+        return str;
+    }
+
+    #creatorOfQuestionMarksForSET(quantity, useCurrentValue) {
+        let str = '';
+        for (let i = 0; i < quantity; i++) {
+            let sign = this.#checkingSomeSymbols(useCurrentValue[i]);
+            let subStr = sign ? '?? ' + sign + ' ?' : '?';
+            i+1 === quantity ? str += `?? = ${subStr}` : str += `?? = ${subStr}, `;
+        }
+        return str;
+    }
+
+    #creatorOfQuestionMarksForTableWithColumn(fields) {
+        let str = '';
+        for (let i = 0; i < fields.length; i++) {
+            str += Array.isArray(fields[i]) ? `??.??${i+1 === fields.length ? '' : ', '}` : `??${i+1 === fields.length ? '' : ', '}`;
+        }
+        return str;
+    }
+
+    async execute(showDetails = false) {
+        let res = await this.#executeQuery(this.#query, this.#values);
+        if (showDetails) {
+            console.log('\r\nОпределения для подстановки: ?? - поле, ? - значение \r\n', 'Массив собранных значений: ', this.#values, '\r\n Строка запроса с метками для подстановки: ', this.#query, '\r\n');
+        }
+        return res;
+    }
+
+    where(field, sign, value, additionalCondition, field2, sign2, value2, additionalCondition2, field3, sign3, value3) {
+        this.#values = this.#values.concat([...[field, value, field2, value2, field3, value3].filter((el) => el !== undefined)]);
+        this.#query += `WHERE ?? ${this.#checkingSomeSymbols(sign)} ? ${this.#checkingSomeSymbols(additionalCondition) ? `${additionalCondition} ${field2 !== undefined ? '??' : 'null'} ${this.#checkingSomeSymbols(sign2)} ${value2 !== undefined ? '?' : 'null'} ${this.#checkingSomeSymbols(additionalCondition2) ? `${additionalCondition2} ${field3 !== undefined ? '??' : 'null'} ${this.#checkingSomeSymbols(sign3)} ${value3 !== undefined ? '?' : 'null'} ` : ''}`: ''}`;
+        return this;
+    }
+
+    groupBy(...fields) {
+        this.#query += `GROUP BY ${this.#creatorOfQuestionMarks(fields.length, true, 'field')} `;
+        this.#values = this.#values.concat(...fields);
+        return this;
+    }
+
+    orderBy(fields, method = 'ASC') {
+        this.#query += `ORDER BY${method === 'RANDOM' ? '' : ` ${this.#creatorOfQuestionMarks(fields.length, true, 'field')}`} ${this.#checkingSomeSymbols(method)} `;
+        if (method !== 'RANDOM') this.#values = this.#values.concat(...fields);
+        return this;
+    }
+
+    limit(quantityOfReturnRows, quantityOfRowsToSkip = 0) {
+        this.#values = this.#values.concat(quantityOfReturnRows, quantityOfRowsToSkip);
+        this.#query += `LIMIT ? OFFSET ? `;
+        return this;
+    }
+
+    select(...fields) {
+        let finallyFieldsArray = [];
+        fields.map(el => Array.isArray(el) ? finallyFieldsArray = finallyFieldsArray.concat(el) : finallyFieldsArray.push(el));
+        this.#query += `SELECT ${finallyFieldsArray[0] === '*' ? '*' : this.#creatorOfQuestionMarksForTableWithColumn(fields)} FROM ${this.options.table} `;
+        if (fields[0] !== '*') this.#values = this.#values.concat(finallyFieldsArray);
+        return this;
+    }
+
+    join(method, tables, firstField, sign, secondField, additionalCondition, firstField2, sign2, secondField2, additionalCondition2, firstField3, sign3, secondField3) {
+        let filteredFieldsArray = [firstField, secondField, firstField2, secondField2, firstField3, secondField3].filter((el) => el !== undefined);
+        let finallyFieldsArray = [];
+        filteredFieldsArray.map(el => Array.isArray(el) ? finallyFieldsArray = finallyFieldsArray.concat(el) : finallyFieldsArray.push(el));
+        this.#values = this.#values.concat([...tables , ...finallyFieldsArray]);
+        this.#query += `${this.#checkingSomeSymbols(method)} JOIN ${this.#creatorOfQuestionMarks(tables.length, true, 'field')} ON ${Array.isArray(firstField) ? `??.??` : '??'} ${this.#checkingSomeSymbols(sign)} ${Array.isArray(secondField) ? `??.??` : '??'} ${this.#checkingSomeSymbols(additionalCondition) ? `${additionalCondition} ${firstField2 !== undefined ? Array.isArray(firstField2) ? `??.??` : '??' : 'null'} ${this.#checkingSomeSymbols(sign2)} ${secondField2 !== undefined ? Array.isArray(secondField2) ? `??.??` : '??' : 'null'} ${this.#checkingSomeSymbols(additionalCondition2) ? `${additionalCondition2} ${firstField3 !== undefined ? Array.isArray(firstField3) ? `??.??` : '??' : 'null'} ${this.#checkingSomeSymbols(sign3)} ${secondField3 !== undefined ? Array.isArray(secondField3) ? `??.??` : '??' : 'null'} ` : ''}`: ''}`;
+        return this;
+    }
+
+    insertValues(fields, values) {
+        let finallyValuesArray = [];
+        values.map(el => finallyValuesArray = finallyValuesArray.concat(el));
+        this.#values = this.#values.concat([...fields, ...finallyValuesArray]);
+        this.#query += `INSERT INTO ${this.options.table} ${this.#creatorOfQuestionMarks(fields.length, true, 'field', true, fields.length)} VALUES ${this.#creatorOfQuestionMarks(finallyValuesArray.length, true, 'value', true, fields.length)} `;
+        return this;
+    }
+
+    updateSet(fields, values, useCurrentValue) {
+        for (let i = 0; i < fields.length; i++) {
+            if (this.#checkingSomeSymbols(useCurrentValue[i])) {
+                this.#values = this.#values.concat([fields[i], fields[i], values[i]]);
+            } else {
+                this.#values = this.#values.concat([fields[i], values[i]]);
+            }
+        }
+        this.#query += `UPDATE ${this.options.table} SET ${this.#creatorOfQuestionMarksForSET(fields.length, useCurrentValue)} `;
+        return this;
+    }
+
+    replaceValues(fields, values) {
+        let finallyValuesArray = [];
+        values.map(el => finallyValuesArray = finallyValuesArray.concat(el));
+        this.#values = this.#values.concat([...fields, ...finallyValuesArray]);
+        this.#query += `REPLACE INTO ${this.options.table} ${this.#creatorOfQuestionMarks(fields.length, true, 'field', true, fields.length)} VALUES ${this.#creatorOfQuestionMarks(finallyValuesArray.length, true, 'value', true, fields.length)} `;
+        return this;
+    }
+
+    delete() {
+        this.#query += `DELETE FROM ${this.options.table} `;
+        return this
+    }
+
+    ownQuery(query, values) {
+        this.#query += query;
+        this.#values = this.#values.concat(values);
+        return this;
+    }
+}
